35870ca3b04a3a6803d8f757b80936cb0b1bca04
Pritam <123.paulpritam@gmail.com>
2025-04-03 20:13:03 +0530

    Initial code created and file setup

---
+++ client/src/components/global/Header.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <nav>
+        <img src="" alt="Logo" />
+        <ul>
+            <li>Chat</li>
+            <li>Friend</li>
+            <li>Search</li>
+        </ul>
+
+        <div>
+            profile
+        </div>
+        <button>
+            Logout
+        </button>
+    </nav>
+  )
+}
+
+export default Header
