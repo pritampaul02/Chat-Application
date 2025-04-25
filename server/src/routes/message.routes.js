a83cb0fd58df23a7369de072e70b32173af08c69
Akash Mondal2025 <adityamondal4567@gmail.com>
2025-04-25 23:51:29 +0530

    message functionality created. edit a message, delete a message, update a message, react to a message,update a react to the message

--- server/src/routes/message.routes.js
+++ server/src/routes/message.routes.js
@@ -1,14 +1,47 @@
-import express from "express"
-import { validate } from "../middlewares/validate.middleware.js"
-import { isAuthenticate } from "../middlewares/authentication.middleware.js"
-import messageValidation from "../validations/message.validation.js"
-import messageController from "../controller/message.controller.js"
+import express from "express";
+import { validate } from "../middlewares/validate.middleware.js";
+import { isAuthenticate } from "../middlewares/authentication.middleware.js";
+import messageValidation from "../validations/message.validation.js";
+import messageController from "../controller/message.controller.js";
 
-const router = express.Router()
+const router = express.Router();
 
 router
-    .post("/send-message" , isAuthenticate , validate(messageValidation.create) , messageController.sendMessage )
-    .get("/:reciverId" , isAuthenticate , validate(messageValidation.getMessage) , messageController.fetchMessage)
+    .post(
+        "/send-message",
+        isAuthenticate,
+        validate(messageValidation.create),
+        messageController.sendMessage
+    )
+    .get(
+        "/:receiverId",
+        isAuthenticate,
+        validate(messageValidation.getMessage),
+        messageController.fetchMessage
+    )
+    .patch(
+        "/edit/:messageId",
+        isAuthenticate,
+        validate(messageValidation.editMessage),
+        messageController.editMessage
+    )
+    .delete(
+        "/:messageId",
+        isAuthenticate,
+        validate(messageValidation.deleteMessage),
+        messageController.deleteMessage
+    )
+    .patch(
+        "/edit/react/:messageId",
+        isAuthenticate,
+        validate(messageValidation.updateReactMessage),
+        messageController.updateReactToMessage
+    )
+    .patch(
+        "react/:messageId",
+        isAuthenticate,
+        validate(messageValidation.reactMessage),
+        messageController.reactToMessage
+    );
 
-    
 export const messageRouter = router;
--- server/src/validations/message.validation.js
+++ server/src/validations/message.validation.js
@@ -1,40 +1,59 @@
-import { z } from "zod"
+import { z } from "zod";
 
 // Define Zod schemas for todo validation
-class MessageValidations  {
-  // Schema for creating a todo
-  create = z.object({
-    body: z.object({
-      message: z.string().trim().min(1, "Title is required"),
-      reciverId:  z.string().trim().min(1, "Reciver ID is required"),
-    }),
-  }) 
+class MessageValidations {
+    // Schema for creating a todo
+    create = z.object({
+        body: z.object({
+            sender: z.string().trim().min(1, "Sender ID is required"),
+            receiver: z.string().trim().min(1, "Reciver ID is required"),
+            receiverModel: z
+                .string()
+                .trim()
+                .min(1, "Receiver model is required"),
+            message: z.string().trim().min(1, "message is required"),
+        }),
+    });
 
-  getMessage = z.object({
-    params: z.object({
-      reciverId: z.string().trim().min(1, "Reciver ID is required"),
-    }),
-  })
+    getMessage = z.object({
+        params: z.object({
+            receiverId: z.string().trim().min(1, "Reciver ID is required"),
+        }),
+    });
 
-  // Schema for updating a todo
-  update = z.object({
-    params: z.object({
-      todoId: z.string().min(1, "Todo ID is required"), // "3" ,  "som4376gf" , "67bb196ba9341c98463a89e9"
-    }),
-    body: z.object({
-      title: z.string().min(1, "Title is required"),
-      description: z.string().optional(),
-      isComplete: z.boolean().optional(),
-    }),
-  })
+    // Schema for updating a message
+    editMessage = z.object({
+        params: z.object({
+            messageId: z.string().trim().min(1, "message ID is required"), // "3" ,  "som4376gf" , "67bb196ba9341c98463a89e9"
+        }),
+        body: z.object({
+            message: z.string().trim().min(1, "message is required"),
+        }),
+    });
 
-  // Schema for deleting a todo
-  delete = z.object({
-    params: z.object({
-      todoId: z.string().min(1, "Todo ID is required"),
-    }),
-  })
+    // Schema for deleting a message
+    deleteMessage = z.object({
+        params: z.object({
+            messageId: z.string().trim().min(1, "message ID is required"),
+        }),
+    });
+    // schema for add reation on a message
+    reactMessage = z.object({
+        params: z.object({
+            messageId: z.string().trim().min(1, "message ID is required"),
+        }),
+        body: z.object({
+            emoji: z.string().trim().min(1, "Emoji is required"),
+        }),
+    });
+    updateReactMessage = z.object({
+        params: z.object({
+            messageId: z.string().trim().min(1, "message ID is required"),
+        }),
+        body: z.object({
+            emoji: z.string().trim().min(1, "Emoji is required"),
+        }),
+    });
 }
 
-export default new MessageValidations()
-
+export default new MessageValidations();
