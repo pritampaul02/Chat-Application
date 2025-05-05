0a84d4f4f2bd54ec2e9a76f4af172c30daab513b
Akash Mondal2025 <adityamondal4567@gmail.com>
2025-05-05 22:51:09 +0530

    cors chage

--- server/src/config/cors.config.js
+++ server/src/config/cors.config.js
@@ -2,9 +2,10 @@ import cors from "cors";
 
 const corsConfig = cors({
     origin: [
+        "https://chat-application-delta-nine.vercel.app/",
         "http://localhost:5173",
         "https://chat-application-client-lime.vercel.app/",
-        "https://chat-application-delta-nine.vercel.app/",
+
         "*/",
     ],
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
