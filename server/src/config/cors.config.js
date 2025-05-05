7ddbc5bb78a70ddcf107c8ebe62cd3515129f426
Akash Mondal2025 <adityamondal4567@gmail.com>
2025-05-05 23:14:38 +0530

    fixed

--- server/src/config/cors.config.js
+++ server/src/config/cors.config.js
@@ -2,9 +2,9 @@ import cors from "cors";
 
 const corsConfig = cors({
     origin: [
-        "https://chat-application-delta-nine.vercel.app/",
+        "https://chat-application-delta-nine.vercel.app",
         "http://localhost:5173",
-        "https://chat-application-client-lime.vercel.app/",
+        "https://chat-application-client-lime.vercel.app",
     ],
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
     credentials: true,
